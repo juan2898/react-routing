67e64de7bbd0220dc35d121866f14a1372d57de7
Juan Jacinto <tgod2898@gmail.com>
2017-08-22 15:06:55 -0600

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-
 import Nav from './Nav/Nav';
 import Cart from './Cart/Cart';
+
+import router from '../router.js'
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Nav />
       <div id="App__mainContainer">
         <div id="App__routerContainer">
+          { router }
           <Cart />
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
-
 import store from './store';
 
 ReactDOM.render(
+  <HashRouter>
   <Provider store={ store }>
     <App />
-  </Provider>, 
-  document.getElementById('root')
+  </Provider>
+  </HashRouter>,
+document.getElementById('root')
 );
-registerServiceWorker();
+// registerServiceWorker();
+
